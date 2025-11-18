bc00a2b22b056cd1e0f5ff30b8669fd94d493d62
neftalito <47830827+neftalito@users.noreply.github.com>
2025-11-18 18:27:02 +0000

    Subo modelo de producto con campos requeridos

--- src/app/models/product.rb
+++ src/app/models/product.rb
@@ -4,4 +4,11 @@ class Product < ApplicationRecord
   belongs_to :category
 
   has_many_attached :images
+  has_many :product_images, dependent: :destroy
+
+  enum :product_type, {vinyl: "viniyl", cd: "cd"}, prefix: true
+  enum :condition, { new: "new", used: "used" }, prefix: true
+
+  validates :author, presence: true
+  validates :inventory_entered_at, :last_modified_at, presence: true
 end
