a242ab85a076b42c610f0f6c1d36074cbd165556
Lautaro Budini <lautarobudini5@gmail.com>
2025-11-22 01:14:01 +0000

    Modificaciones en los modelos

--- src/app/models/product.rb
+++ src/app/models/product.rb
@@ -6,12 +6,41 @@ class Product < ApplicationRecord
   has_many_attached :images
   has_one_attached :audio
 
-  enum :product_type, {vinyl: "viniyl", cd: "cd"}, prefix: true
+  enum :product_type, { vinyl: "vinyl", cd: "cd" }, prefix: true
   enum :condition, { new: "new", used: "used" }, prefix: true
-  enum :product_state, { new_disc: 0, used_disc: 1 }
+
   # Scope para productos no borrados lógicamente
   scope :available_products, -> { where(deleted_at: nil) }
 
   validates :author, presence: true
   validates :inventory_entered_at, :last_modified_at, presence: true
+
+  validate :must_have_at_least_one_image
+  validate :audio_only_for_used_products
+
+  # Callback: si el producto cambia a nuevo, eliminar el audio
+  before_validation :remove_audio_if_new
+
+  private
+
+  def must_have_at_least_one_image
+    # En creación o edición, debe tener al menos 1 imagen
+    unless images.attached? && images.any?
+      errors.add(:images, "debe tener al menos una imagen")
+    end
+  end
+
+  def audio_only_for_used_products
+    # Solo productos usados pueden tener audio
+    if audio.attached? && condition == "new"
+      errors.add(:audio, "solo puede adjuntarse a productos usados")
+    end
+  end
+
+  def remove_audio_if_new
+    # Si el estado cambió de used a new, purgar el audio automáticamente
+    if condition_changed? && condition == "new" && audio.attached?
+      audio.purge
+    end
+  end
 end
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_18_224209) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_21_231817) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -108,7 +108,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_18_224209) do
     t.datetime "last_modified_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.string "name"
     t.decimal "price"
-    t.integer "product_state", default: 0, null: false
     t.string "product_type", default: "vinyl", null: false
     t.boolean "published"
     t.integer "stock"
