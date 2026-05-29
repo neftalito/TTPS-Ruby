a3309f2f197a9851370eefc8b8fdb31e7bdcd4b0
neftalito <47830827+neftalito@users.noreply.github.com>
2026-05-29 17:33:09 +0000

    Mejoro tests de localización

--- src/test/controllers/locales_controller_test.rb
+++ src/test/controllers/locales_controller_test.rb
@@ -1,28 +1,42 @@
 require "test_helper"
 
 class LocalesControllerTest < ActionDispatch::IntegrationTest
-  test "updates locale in session for guests" do
-    patch locale_url, params: { locale: "en", return_to: storefront_products_path }
+  test "updates locale for guests through the rendered storefront" do
+    patch locale_url, params: { locale: "en", return_to: root_path }
 
-    assert_redirected_to storefront_products_url
-    assert_equal "en", session[:locale]
+    assert_redirected_to root_url
+
+    follow_redirect!
+
+    assert_response :success
+    assert_includes response.body, I18n.t("common.navigation.home", locale: :en)
+    assert_includes response.body, I18n.t("common.navigation.catalog", locale: :en)
   end
 
-  test "updates locale for signed in users" do
+  test "updates locale for signed in users and persists it" do
     user = users(:employee)
     sign_in user
 
     patch locale_url, params: { locale: "en", return_to: backstore_root_path }
 
     assert_redirected_to backstore_root_url
-    assert_equal "en", session[:locale]
     assert_equal "en", user.reload.locale
+
+    follow_redirect!
+
+    assert_response :success
+    assert_includes response.body, I18n.t("layouts.backstore.heading", locale: :en)
   end
 
-  test "falls back to default locale when an unsupported value is submitted" do
+  test "falls back to the default locale when an unsupported value is submitted" do
     patch locale_url, params: { locale: "pt", return_to: root_path }
 
     assert_redirected_to root_url
-    assert_equal I18n.default_locale.to_s, session[:locale]
+
+    follow_redirect!
+
+    assert_response :success
+    assert_includes response.body, I18n.t("common.navigation.home", locale: I18n.default_locale)
+    assert_includes response.body, I18n.t("common.navigation.catalog", locale: I18n.default_locale)
   end
 end
